2069313f6a8b7591129dc59fbc1506e7ce222e9c
celhinho <celio.c.n.junior@hotmail.com>
2024-07-30 16:01:44 -0300

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,50 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #000;
+    color: #fff;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    background-color: #000;
+    padding: 20px;
+    text-align: center;
+    border-bottom: 3px solid #ff0000;
+}
+
+h1 {
+    color: #ff0000;
+}
+
+main {
+    padding: 20px;
+}
+
+h2 {
+    color: #ff0000;
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+.slider {
+    position: relative;
+    width: 80%;
+    margin: 0 auto;
+    overflow: hidden;
+}
+
+.slide {
+    display: none;
+}
+
+.slide img {
+    width: 100%;
+}
+
+footer {
+    background-color: #000;
+    text-align: center;
+    padding: 10px;
+    border-top: 3px solid #ff0000;
+}
